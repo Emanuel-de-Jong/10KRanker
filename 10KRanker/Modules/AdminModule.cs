d8172b3b178babe75d2f06f1a434d0509efe06c0
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-06-30 22:08:59 +0200

    Initial commit

---
+++ 10KRanker/Modules/AdminModule.cs
@@ -0,0 +1,22 @@
+﻿
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using _10KRanker.Services;
+
+namespace _10KRanker.Modules
+{
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ban")]
+        [RequireContext(ContextType.Guild)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        public async Task BanUserAsync(IGuildUser user, [Remainder] string reason = null)
+        {
+            await user.Guild.AddBanAsync(user, reason: reason);
+            await ReplyAsync("ok!");
+        }
+    }
+}
---
+++ 10KRanker/Modules/PublicModule.cs
@@ -0,0 +1,58 @@
+﻿
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using _10KRanker.Services;
+
+namespace _10KRanker.Modules
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        public PictureService PictureService { get; set; }
+
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        public Task PingAsync()
+            => ReplyAsync("pong!");
+
+
+
+        [Command("cat")]
+        public async Task CatAsync()
+        {
+            var stream = await PictureService.GetCatPictureAsync();
+            stream.Seek(0, SeekOrigin.Begin);
+            await Context.Channel.SendFileAsync(stream, "cat.png");
+        }
+
+
+
+        [Command("userinfo")]
+        public async Task UserInfoAsync(IUser user = null)
+        {
+            user = user ?? Context.User;
+
+            await ReplyAsync(user.ToString());
+        }
+
+
+
+        [Command("echo")]
+        public Task EchoAsync([Remainder] string text)
+            => ReplyAsync('\u200B' + text);
+
+
+
+        [Command("list")]
+        public Task ListAsync(params string[] objects)
+            => ReplyAsync("You listed: " + string.Join("; ", objects));
+
+
+
+        [Command("guild_only")]
+        [RequireContext(ContextType.Guild, ErrorMessage = "Sorry, this command must be ran from within a server, not a DM!")]
+        public Task GuildOnlyCommand()
+            => ReplyAsync("Nothing to see here!");
+    }
+}
