dde1bcc7ec8dd00cfce287963419174c4e843cb4
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-07-09 16:56:36 +0200

    Osu log

--- src/OsuAPI/Osu.cs
+++ src/OsuAPI/Osu.cs
@@ -1,18 +1,26 @@
-﻿using OsuSharp;
+﻿using Logger;
+using OsuSharp;
 using System;
 
 namespace OsuAPI
 {
     public static class Osu
     {
+        private static Log log = new Log("osu");
         private static readonly OsuClient client = new(new OsuSharpConfiguration() { ApiKey = Secrets.ApiKey });
         public static OsuClient Client { get; }
 
         public static Beatmap GetMap(long beatmapsetId)
         {
+            string logMessage = $"GetMap({ beatmapsetId });";
+
             var maps = client.GetBeatmapsetAsync(beatmapsetId).Result;
+
             if (maps.Count == 0)
+            {
+                log.Write(logMessage, "not found");
                 throw new ArgumentException("A map with that id does not exist.");
+            }
 
             Beatmap map = null;
             foreach (Beatmap m in maps)
@@ -25,31 +33,56 @@ namespace OsuAPI
             }
 
             if (map == null)
+            {
+                log.Write(logMessage, "not valid");
                 throw new ArgumentException("The map doesn't have a 10K difficulty.");
+            }
 
             if (map.State == BeatmapState.Ranked)
+            {
+                log.Write(logMessage, "not valid");
                 throw new ArgumentException("The map is already ranked.");
+            }
 
             if (map.DownloadUnavailable || map.AudioUnavailable)
+            {
+                log.Write(logMessage, "not valid");
                 throw new ArgumentException("The map is corrupted.");
+            }
+
+            log.Write(logMessage);
 
             return map;
         }
 
         public static User GetUser(long userId)
         {
+            string logMessage = $"GetUser({ userId });";
+
             User user = client.GetUserByUserIdAsync(userId, GameMode.Mania).Result;
             if (user == null)
+            {
+                log.Write(logMessage, "not found");
                 throw new ArgumentException("A user with that id doesn't exist.");
+            }
+
+            log.Write(logMessage);
 
             return user;
         }
 
         public static User GetUser(string userName)
         {
+            string logMessage = $"GetUser(\"{ userName }\");";
+
             User user = client.GetUserByUsernameAsync(userName, GameMode.Mania).Result;
             if (user == null)
+            {
+                log.Write(logMessage, "not found");
                 throw new ArgumentException("A user with that name doesn't exist.");
+            }
+
+            log.Write(logMessage);
 
             return user;
         }
